c5bf9b85323a36e320aab4f4cc00c542562af1cb
Ziiomc <131203674+Ziiomc@users.noreply.github.com>
2026-08-20 16:34:52 -0400

    Show daily service controls in Drivers dashboard

--- src/components/modules/DriverApplicationsPanel.tsx
+++ src/components/modules/DriverApplicationsPanel.tsx
@@ -1,1 +1,6 @@
-import React from'react';import{DriverInviteLinkPanel}from'./DriverInviteLinkPanel';import{DriverApplicationsPanel as Base}from'./DriverApplicationsPanelBase';export const DriverApplicationsPanel:React.FC<{companyId:string}>=({companyId})=><div><DriverInviteLinkPanel companyId={companyId}/><Base companyId={companyId}/></div>;
+import React from'react';
+import{DriverInviteLinkPanel}from'./DriverInviteLinkPanel';
+import{DriverApplicationsPanel as Base}from'./DriverApplicationsPanelBase';
+import{DriverDailyServicePanel}from'./DriverDailyServicePanel';
+
+export const DriverApplicationsPanel:React.FC<{companyId:string}>=({companyId})=><div className="space-y-4"><DriverDailyServicePanel companyId={companyId}/><DriverInviteLinkPanel companyId={companyId}/><Base companyId={companyId}/></div>;
