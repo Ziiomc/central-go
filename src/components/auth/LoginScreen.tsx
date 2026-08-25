9f0a31b1b379d30d32fd9baa4362c5a8077fd9af
Ziiomc <131203674+Ziiomc@users.noreply.github.com>
2026-08-25 11:04:01 -0400

    feat: route authorized workstations to operator login

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { readDriverInviteCodeFromUrl } from '../../lib/driverInvite';
+import { readOperatorTerminal } from '../../lib/operatorTerminal';
 import { DriverInviteAuthScreen } from './DriverInviteAuthScreen';
+import { OperatorTerminalLogin } from './OperatorTerminalLogin';
 import { LoginScreen as StandardLoginScreen } from './LoginScreenBase';
 
 export const LoginScreen: React.FC = () => {
   const inviteCode = readDriverInviteCodeFromUrl();
-  return inviteCode ? <DriverInviteAuthScreen inviteCode={inviteCode} /> : <StandardLoginScreen />;
+  const terminal = useMemo(() => readOperatorTerminal(), []);
+  const [adminAccess, setAdminAccess] = useState(false);
+
+  if (inviteCode) return <DriverInviteAuthScreen inviteCode={inviteCode} />;
+  if (terminal && !adminAccess) return <OperatorTerminalLogin terminal={terminal} onAdminAccess={() => setAdminAccess(true)} />;
+  return <StandardLoginScreen />;
 };
